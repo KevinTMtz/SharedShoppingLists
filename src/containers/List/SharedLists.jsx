e7eb8e04a2d74fd45e1a6f9e65dd4b375cea2822
KevinTMtz <torresmtzkevin@gmail.com>
2021-10-27 18:58:13 -0500

    Display only current user lists

--- src/containers/List/SharedLists.jsx
+++ src/containers/List/SharedLists.jsx
@@ -1,28 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import axios from 'axios';
 import { getAuth, signOut } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 import Button from '../../components/Button/Button';
 import ButtonsDiv from '../../components/Button/ButtonsDiv';
 import ListCell from '../../components/List/ListCell';
-import axios from 'axios';
 
 const SharedLists = () => {
   const history = useHistory();
 
   const auth = getAuth();
+  const [user] = useAuthState(auth);
 
-  const [lists, setLists] = useState({ listID: { listName: 'List Name' } });
+  const [lists, setLists] = useState({
+    listID: { listName: 'List Name', list: [] },
+  });
 
   useEffect(
     () =>
       axios
-        .get('https://sharedlist-d718d-default-rtdb.firebaseio.com/lists.json')
-        .then((result) => {
-          setLists(result.data);
+        .get(
+          `https://sharedlist-d718d-default-rtdb.firebaseio.com/usersLists/${user.uid}.json`,
+        )
+        .then((usersListsData) => {
+          axios
+            .get(
+              'https://sharedlist-d718d-default-rtdb.firebaseio.com/lists.json',
+            )
+            .then((result) => {
+              const userLists = {};
+              Object.keys(result.data).forEach((listId) => {
+                if (
+                  usersListsData.data.lists.indexOf(listId) > -1 &&
+                  Object.keys(userLists).indexOf(listId) === -1
+                ) {
+                  userLists[listId] = result.data[listId];
+                }
+              });
+
+              setLists(userLists);
+            })
+            .catch((error) => console.log(error));
         })
         .catch((error) => console.log(error)),
-    [],
+    [user.uid],
   );
 
   const logout = async () => {
@@ -41,7 +64,8 @@ const SharedLists = () => {
 
   return (
     <div>
-      <h1>Shared Lists</h1>
+      <h1 style={{ textAlign: 'center' }}>Shopping Lists</h1>
+      <h4 style={{ textAlign: 'center' }}>{user.email}</h4>
       <ButtonsDiv>
         <Button onClick={() => history.push('/create-list')}>
           Create or Add list
@@ -57,7 +81,8 @@ const SharedLists = () => {
               key={`list-element-${lists[listElement].listName}-${index}`}
               onClick={() => goToList(listElement)}
             >
-              {lists[listElement].listName}
+              {lists[listElement].listName} - Items:{' '}
+              {lists[listElement].list?.length ?? 0}
             </ListCell>
           );
         })}
